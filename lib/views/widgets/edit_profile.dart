6cbb27bed57e259b81e12df19fbd1ba7cebc17aa
JuliaAlos <73183242+JuliaAlos@users.noreply.github.com>
2022-06-06 12:52:15 +0200

    Edit profile (#41)

--- lib/views/widgets/edit_profile.dart
+++ lib/views/widgets/edit_profile.dart
@@ -1,9 +1,13 @@
 import 'dart:developer';
 
 import 'package:date_time_picker/date_time_picker.dart';
+import 'package:ea_frontend/models/author.dart';
 import 'package:ea_frontend/models/user.dart';
+import 'package:ea_frontend/routes/author_service.dart';
+import 'package:ea_frontend/routes/book_service.dart';
 import 'package:ea_frontend/routes/user_service.dart';
 import 'package:ea_frontend/views/provider/theme_provider.dart';
+import 'package:ea_frontend/views/widgets/writer_add_book.dart';
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:email_validator/email_validator.dart';
@@ -20,13 +24,19 @@ class EditProfile extends StatefulWidget {
 
 class _EditProfileState extends State<EditProfile> {
   late String idUser;
+  var author;
   var storage;
   var isEditing = false;
+  String biograpy = "";
+  TextEditingController _textFieldController = TextEditingController();
 
   TextEditingController controllerName = TextEditingController(text: 'Name');
   TextEditingController controllerUserName =
       TextEditingController(text: 'userName');
   TextEditingController controllerMail = TextEditingController(text: 'mail');
+  TextEditingController controllerBiography =
+      TextEditingController(text: 'biography');
+
   String controllerBirthDay = "";
 
   Future<User> fetchUser() async {
@@ -34,6 +44,11 @@ class _EditProfileState extends State<EditProfile> {
     await storage.ready;
 
     idUser = LocalStorage('BookHub').getItem('userId');
+    author = await AuthorService.getAuthor(idUser);
+    if (author.runtimeType == Author) {
+      LocalStorage('BookHub').setItem('idAuthor', author.id);
+    }
+
     return UserService.getUser(idUser);
   }
 
@@ -47,8 +62,17 @@ class _EditProfileState extends State<EditProfile> {
             controllerUserName.text = snapshot.data!.userName;
             controllerMail.text = snapshot.data!.mail;
             controllerBirthDay = snapshot.data!.birthDate.toString();
+            if (author.runtimeType == Author) {
+              controllerBiography.text = author.biography;
+            } else {
+              author == null;
+            }
+
             return Scaffold(
               appBar: AppBar(
+                title: Text(
+                  "Edit Profile",
+                ),
                 backgroundColor: Theme.of(context).scaffoldBackgroundColor,
                 elevation: 1,
                 leading: IconButton(
@@ -69,11 +93,6 @@ class _EditProfileState extends State<EditProfile> {
                 padding: EdgeInsets.only(left: 16, top: 25, right: 16),
                 child: ListView(
                   children: [
-                    Text(
-                      "Edit Profile",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-                    ),
                     SizedBox(
                       height: 15,
                     ),
@@ -129,11 +148,7 @@ class _EditProfileState extends State<EditProfile> {
                       ),
                     ),
                     SizedBox(
-                      height: 35,
-                    ),
-                    buildData(snapshot),
-                    SizedBox(
-                      height: 35,
+                      height: 25,
                     ),
                     (isEditing
                         ? Row(
@@ -164,6 +179,20 @@ class _EditProfileState extends State<EditProfile> {
                                         controllerUserName.text,
                                         controllerMail.text,
                                         controllerBirthDay);
+
+                                    if (author != null &&
+                                        controllerBiography
+                                            .value.text.isNotEmpty) {
+                                      var response2 =
+                                          await AuthorService.updateAuthor(
+                                              author.id,
+                                              controllerName.text,
+                                              controllerBiography.text,
+                                              controllerBirthDay,
+                                              author.deathDate.toString(),
+                                              author.photoURL);
+                                    }
+
                                     if (response) {
                                       setState(() {
                                         isEditing = false;
@@ -206,7 +235,115 @@ class _EditProfileState extends State<EditProfile> {
                               )
                             ],
                           )
-                        : Container()),
+                        : Container(
+                            height: 10,
+                          )),
+                    buildData(snapshot),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    (author == null
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              _buildSeparator(),
+                              Container(
+                                height: 40,
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                  color: Colors.green,
+                                ),
+                                child: InkWell(
+                                    child: Row(
+                                      children: [
+                                        Text("Author information"),
+                                        Icon(
+                                          Icons.add,
+                                          color: Colors.white,
+                                        ),
+                                      ],
+                                    ),
+                                    onTap: () {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title:
+                                                  Text('Enter your biography'),
+                                              content: Container(
+                                                width: MediaQuery.of(context)
+                                                        .size
+                                                        .width /
+                                                    1.5,
+                                                child: TextFormField(
+                                                  maxLines: 10,
+                                                  maxLength: 1000,
+                                                  strutStyle: StrutStyle(),
+                                                  onChanged: (value) {
+                                                    setState(() {
+                                                      biograpy = value;
+                                                    });
+                                                  },
+                                                  controller:
+                                                      _textFieldController,
+                                                  decoration: InputDecoration(
+                                                      hintText: "My life..."),
+                                                ),
+                                              ),
+                                              actions: <Widget>[
+                                                FlatButton(
+                                                  color: Colors.green,
+                                                  textColor: Colors.white,
+                                                  child: Text('OK'),
+                                                  onPressed: () async {
+                                                    String category = snapshot
+                                                        .data!
+                                                        .categories[0]
+                                                        .name!;
+                                                    for (int i = 1;
+                                                        i <
+                                                            snapshot
+                                                                .data!
+                                                                .categories
+                                                                .length;
+                                                        i++) {
+                                                      category = category +
+                                                          "," +
+                                                          snapshot
+                                                              .data!
+                                                              .categories[i]
+                                                              .name!;
+                                                    }
+                                                    await AuthorService
+                                                        .postAuthor(
+                                                            idUser,
+                                                            snapshot.data!.name,
+                                                            biograpy,
+                                                            snapshot.data!.mail,
+                                                            snapshot
+                                                                .data!.birthDate
+                                                                .toString(),
+                                                            snapshot
+                                                                .data!.birthDate
+                                                                .toString(),
+                                                            category,
+                                                            snapshot.data!
+                                                                .photoURL);
+                                                    setState(() {
+                                                      Navigator.pop(context);
+                                                    });
+                                                  },
+                                                ),
+                                              ],
+                                            );
+                                          });
+                                    }),
+                              ),
+                              _buildSeparator()
+                            ],
+                          )
+                        : buildDataAuthor()),
                   ],
                 ),
               ),
@@ -241,12 +378,143 @@ class _EditProfileState extends State<EditProfile> {
             icon: const Icon(Icons.event),
             dateLabelText: "birthDate",
             onSaved: (val) => controllerBirthDay = val!,
-            onChanged: (val) => controllerBirthDay = val,
+            onChanged: (val) => {
+              controllerBirthDay = val,
+              (!isEditing)
+                  ? setState(() {
+                      isEditing = true;
+                    })
+                  : null
+            },
             onFieldSubmitted: (val) => controllerBirthDay = val,
           ),
         )
       ],
     );
+  }
+
+  Widget buildDataAuthor() {
+    return Column(
+      children: [
+        _buildSeparatorBig(),
+        buildEditBig("Biography", controllerBiography),
+        Text("Books"),
+        SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: booksList(),
+            )),
+        ElevatedButton(
+          onPressed: () async {
+            bool response = await AuthorService.deleteAuthor(author.id);
+            if (response) {
+              setState(() {});
+            }
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+          ),
+          child: Text(
+            "Delete author",
+            style: TextStyle(
+                fontSize: 14, letterSpacing: 2.2, color: Colors.white),
+          ),
+        ),
+        Container(
+          height: 10,
+        )
+      ],
+    );
+  }
+
+  List<Widget> booksList() {
+    List<Widget> list = [];
+    author.books.forEach((element) {
+      list.add(buildBook(
+          element.title,
+          element.publishedDate.day.toString() +
+              "/" +
+              element.publishedDate.month.toString() +
+              "/" +
+              element.publishedDate.year.toString(),
+          element.id));
+    });
+    list.add(InkWell(
+        onTap: () => {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const AddBook()))
+            },
+        child: Container(
+          width: 300,
+          height: 150,
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            margin: EdgeInsets.all(15),
+            elevation: 10,
+            child: const Icon(Icons.add),
+          ),
+        )));
+    return list;
+  }
+
+  Widget buildBook(String title, String published, String bookId) {
+    return Container(
+        width: 300,
+        height: 150,
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          margin: EdgeInsets.all(15),
+          elevation: 10,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
+                title: Text(title),
+                subtitle: Text("Published Date: " + published),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(onPressed: () => {}, child: Text('Edit')),
+                  Container(
+                    width: 30,
+                  ),
+                  ElevatedButton(
+                    child: Text("Delete"),
+                    onPressed: () => showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Delete book?'),
+                        content: const Text(
+                            'Permanently delete or change the author to anonymous?'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => {
+                              BookService.deleteBook(bookId),
+                              Navigator.pop(context, 'Delete'),
+                              setState(() {})
+                            },
+                            child: const Text('Delete'),
+                          ),
+                          TextButton(
+                            onPressed: () => {
+                              AuthorService.deleteBook(bookId, author.id),
+                              Navigator.pop(context, 'Anonymous'),
+                              setState(() {})
+                            },
+                            child: const Text("Anonymous"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 
   Widget buildEdit(String labelText, TextEditingController controller) {
@@ -274,4 +542,53 @@ class _EditProfileState extends State<EditProfile> {
       ),
     );
   }
+
+
+  Widget buildEditBig(String labelText, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextFormField(
+        controller: controller,
+        maxLines: 10,
+        maxLength: 1000,
+        strutStyle: StrutStyle(),
+        onTap: () {
+          (!isEditing)
+              ? setState(() {
+                  isEditing = true;
+                })
+              : null;
+        },
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: labelText,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).shadowColor,
+            )),
+      ),
+    );
+  }
+
+  Widget _buildSeparator() {
+    return Container(
+      width: MediaQuery.of(context).size.width / 4,
+      height: 2.0,
+      color: Colors.black54,
+      margin: const EdgeInsets.only(top: 6, bottom: 6),
+    );
+  }
+
+  Widget _buildSeparatorBig() {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2,
+      height: 2.0,
+      color: Colors.black54,
+      margin: const EdgeInsets.only(top: 6, bottom: 6),
+    );
+  }
+
 }
